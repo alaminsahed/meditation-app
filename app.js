f065f49c48361e1ca73dc5547685b5f2696d87ee
Al-amin Sahed <alaminsahed97@gmail.com>
2020-02-28 00:57:02 +0600

    update appjs

--- app.js
+++ app.js
@@ -3,7 +3,52 @@ const song = document.querySelector('.song');
 const play = document.querySelector('.play');
 const outline = document.querySelector('.moving-outline circle');
 const video = document.querySelector('.vid-container video');
-}
+
+const sounds = document.querySelectorAll(".sound-picker button");
+const timeSelect = document.querySelectorAll(".time-select button");
+
+const timeDisplay = document.querySelector(".display-time");
+
+const outlineLength = outline.getTotalLength();
+
+let duration = 600;
+
+outline.style.strokeDasharray = outlineLength;
+outline.style.strokeDashoffset = outlineLength;
+
+play.addEventListener("click",()=>{
+    checkPlaying(song);
+});
+
+//timeSelect.forEach(Option =>)
+
+const checkPlaying = song =>{
+    if (song.paused){
+        song.play();
+        video.play();
+        play.src="./svg/pause.svg"
+    }
+    else{
+        song.pause();
+        video.pause();
+        play.src="./svg/play.svg"
+    }
+};
+
+song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = duration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    let progress = outlineLength - (currentTime / duration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+
+    timeDisplay.textContent= `${minutes}:${seconds}`;
+};
+
+};
+
 
 
 app();
